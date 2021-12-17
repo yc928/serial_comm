c255dd55672ade189fc4681ecbe27476c6709d49
YiChung Lin <ianlin79928@gmail.com>
2021-12-17 21:48:29 +0800

    Rename package

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-package_name = 'serial_sever'
+package_name = 'serial_comm'
 
 setup(
     name=package_name,
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'serial_sever_node = serial_sever.serial_sever_node:main'
+            'serial_sever_node = serial_comm.serial_sever_node:main'
         ],
     },
 )
